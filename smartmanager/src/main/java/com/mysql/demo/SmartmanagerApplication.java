0377566adde21f6247c9c1abf101d50486f5f21a
rohitshelar <shelarrohit78@gmail.com>
2021-02-21 12:14:34 +0530

    smart manager

---
+++ smartmanager/src/main/java/com/mysql/demo/SmartmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmanagerApplication.class, args);
+	}
+
+}
